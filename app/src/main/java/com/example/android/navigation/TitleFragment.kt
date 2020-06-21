d0feb6b3fefcc846721f65112cbdcf6432ef69b9
gr3y6h0st <jayvy19@gmail.com>
2020-06-20 19:21:57 -0700

    added Navigation Graph and Components to build. Added title and game fragments to navigation graph. Connected Title and Game Fragments with an action.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -14,6 +16,16 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+
+        //The complete onClickListener with Navigation - Lambda Style (Anonymous function)
+        /*binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }*/
+
+        //The complete onClickListener with Navigation using createNavigateOnClickListener
+        binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+
         return binding.root
     }
 }
