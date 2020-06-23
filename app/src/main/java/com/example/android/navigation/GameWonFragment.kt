c35afe3ff9c57eab45fe9caafd58859058ecd858
gr3y6h0st <jayvy19@gmail.com>
2020-06-22 18:43:15 -0700

    Implemented Share Button on GameWonFragment using Implicit Intent.

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,10 +16,9 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -38,8 +37,34 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener{
             view: View-> view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
-        val args = arguments?.let { GameWonFragmentArgs.fromBundle(it) }
-        Toast.makeText(context, "NumCorrect: ${args?.numCorrect},NumQuestions: ${args?.numQuestions}",Toast.LENGTH_LONG).show()
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+        if(null == getShareIntent().resolveActivity(activity!!.packageManager)){
+            //hide menu item if it doesn't resolve
+            menu.findItem(R.id.share)?.isVisible = false
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getShareIntent(): Intent {
+        val args = arguments?.let { GameWonFragmentArgs.fromBundle(it) }
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args?.numCorrect,args?.numQuestions))
+        Toast.makeText(context, "NumCorrect: ${args?.numCorrect},NumQuestions: ${args?.numQuestions}",Toast.LENGTH_LONG).show()
+        return shareIntent
+    }
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
     }
 }
